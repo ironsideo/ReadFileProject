792297d2c33e85d73e07ae017211fd4ce2441a37
io37@njit.edu <Wendy#@1>
2019-03-10 16:45:30 -0400

    Task: moving the index file to the correct folder

--- public/index.php
+++ public/index.php
@@ -3,22 +3,7 @@
  * Created by PhpStorm.
  * User: ironside
  * Date: 3/10/19
- * Time: 4:32 PM
+ * Time: 2:45 PM
  */
 
-main::start();
-
-class main {
-
-    static public function start() {
-
-        $file = fopen( "datafile.cs", "r");
-
-        while(! feof($file))
-        {
-            print_r(fgetcsv($file));
-        }
-
-        fclose($file);
-    }
-}
+echo 'test123 Moretestingggty';
