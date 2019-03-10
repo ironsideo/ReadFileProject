8ae0dc14068d5cfa9289220ac174272e84991d5b
io37@njit.edu <Wendy#@1>
2019-03-10 16:39:53 -0400

    Task: adding data file

--- index.php
+++ index.php
@@ -7,3 +7,18 @@
  */
 
 main::start();
+
+calss main {
+
+    static public function start() {
+
+        $file = fopen( "datafile.cs", "r");
+
+        while(! feof($file))
+        {
+            print_r(fgetcsv($file));
+        }
+
+        fclose($file);
+    }
+}
