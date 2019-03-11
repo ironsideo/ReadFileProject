8b3a899a60d7e194058af91c901244bcd82482a8
io37@njit.edu <Wendy#@1>
2019-03-10 22:37:10 -0400

    Task: moving the index file to the correct folder

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php
+<<?php
 /**
  * Created by PhpStorm.
  * User: ironside
@@ -7,4 +7,34 @@
  */
 
 
-echo "tes";
+// main::start("DataFile.csv");
+main::start("DataFile.csv");
+
+class main  {
+
+    static public function start($filename)
+    {
+        $records = csv::getRecords($filename);
+        print_r($records);
+    }
+}
+
+class csv
+{
+    static public function getRecords($filename)
+    {
+        if($file = fopen( $filename, "r" ))
+        {
+            $record = fgetcsv( $file );			// Headers
+            $data = Array( $record );			// Put on the table
+            while (!feof( $file )) {
+                $record = fgetcsv( $file );
+                array_push( $data, $record );
+            }
+            fclose( $file );
+            return $data;
+        }
+    }
+}
+
+
